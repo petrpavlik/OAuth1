bedd790c78bd3a27f0438c3d7c7841db455d2def
Petr Pavlik <pavlipe7@gmail.com>
2017-01-21 16:11:58 +0100

    Fix crypto dependency by bumping it

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "OAuth1",
     dependencies: [
         // HMAC-SHA1
-        .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", majorVersion: 0)
     ]
 )
